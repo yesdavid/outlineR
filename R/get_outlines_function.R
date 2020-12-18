f675182e1b3bfb6cef9d426ef75d4c4ec815329a
yesdavid <30009559+yesdavid@users.noreply.github.com>
2020-12-19 00:50:17 +0100

    paste0() replaces by file.path()

--- R/get_outlines_function.R
+++ R/get_outlines_function.R
@@ -24,24 +24,19 @@ get_outlines <- function(pathname_output) {
   pb2 <- utils::txtProgressBar(min = 0, max = length(artefact_names), style = 3)
   for (input_counter in 1:length(artefact_names)) {
 
-    # per Momocs werden die outlines eines jpgs geladen
-    outline_coordinates <- Momocs::import_jpg1(paste0(pathname_output, "/", artefact_names[input_counter]))
-    out_file_single_outline <- Momocs::Out(outline_coordinates) # In Momocs, Out-classes objects are lists of closed outlines, with optional components, and on which generic methods such as plotting methods (e.g. stack) and specific methods (e.g. efourier can be applied. Out objects are primarily Coo objects.
-    # Momocs::inspect(out_file_single_outline)
+    outline_coordinates <- Momocs::import_jpg(file.path(pathname_output, artefact_names[input_counter]))
+    out_file_single_outline <- Momocs::Out(outline_coordinates)
 
     # Rescale coordinates from pixels to real length units
     # current_tps_dataset <- subset(tps_file_regex, IMAGE == artefact_names[input_counter])
-
     ## rescale using rescale factor
-    # out_file_single_outline_rescaled <- Momocs::rescale(out_file_single_outline,
-    #                                  scaling_factor = current_tps_dataset$SCALE)
-    # names(out_file_single_outline_rescaled) <- strsplit(artefact_names[input_counter], split ='[.]')[[1]][1]
-    # Momocs::inspect(out_file_single_outline_rescaled)
+    # out_file_single_outline <- Momocs::rescale(out_file_single_outline,
+    #                                            scaling_factor = current_tps_dataset$SCALE)
 
-    # outlines[[gsub("-", "_",  strsplit(artefact_names[input_counter], split ='[.]')[[1]][1])]] <- out_file_single_outline_rescaled
-
-    names(out_file_single_outline) <- strsplit(artefact_names[input_counter], split = "[.]")[[1]][1]
-    outlines[[gsub("-", "_", strsplit(artefact_names[input_counter], split = "[.]")[[1]][1])]] <- out_file_single_outline
+    names(out_file_single_outline) <- strsplit(artefact_names[input_counter],
+                                               split = "[.]")[[1]][1]
+    outlines[[gsub("-", "_", strsplit(artefact_names[input_counter],
+                                      split = "[.]")[[1]][1])]] <- out_file_single_outline
 
     utils::setTxtProgressBar(pb2, input_counter)
   }
--- R/separate_single_artefacts_function.R
+++ R/separate_single_artefacts_function.R
@@ -22,48 +22,21 @@ separate_single_artefacts <- function(pathname_input, pathname_output) {
   for (current_masked_file in 1:length(pathname_input)) {
     x_raw <- imager::load.image(pathname_input[current_masked_file])
 
-    # plot(x_raw)
-
     x_grayscale <- imager::grayscale(x_raw)
-    # plot(x_grayscale)
-
-    # x_binary <- round(x_grayscale/max(x_grayscale),0)
-    # plot(x_binary)
 
     # because some images had to be pre-processed in GIMP with the thresholding tool, this step might result in errors
-    thresholding_error <- testit::has_error(imager::threshold(x_grayscale),
-      silent = T
-    )
-    if (thresholding_error == FALSE) {
-      x_threshold <- imager::threshold(x_grayscale)
-    } else {
-      x_threshold <- x_grayscale
-    }
-
+    x_threshold <- x_grayscale
+    try(x_threshold <- imager::threshold(x_threshold),
+        silent = TRUE)
     x_clean <- imager::clean(x_threshold, 10)
-    # plot(x_clean)
-
-
     x_clean_cimg <- imager::as.cimg(x_clean)
+
     x_clean_img <- EBImage::Image(x_clean_cimg)
-    # EBImage::display(x_clean_img,
-    #                  method = "raster",
-    #                  all = T)
-
     x_clean_img_filled <- EBImage::fillHull(1 - x_clean_img)
-    # EBImage::display(x_clean_img_filled,
-    #                  method = "raster",
-    #                  all = T)
-
 
     bin_image <- round(x_clean_img_filled / max(x_clean_img_filled), 0)
 
     bin_image_labeled <- EBImage::bwlabel(bin_image)
-
-    # EBImage::display(bin_image_labeled,
-    #                  method = "raster",
-    #                  all = T)
-
 
     # important step to be able to work with images that are already binary and images that are still in grayscale
     if (EBImage::numberOfFrames(bin_image_labeled) == 1) {
@@ -76,24 +49,17 @@ separate_single_artefacts <- function(pathname_input, pathname_output) {
       )
     }
 
-    # EBImage::computeFeatures.shape(bin_image_labeled_filled_frame) %>% EBImage::display(method = "raster", all = T)
-
     features <- EBImage::computeFeatures.shape(bin_image_labeled_filled_frame)
-
 
     all_objects <- 1:max(bin_image_labeled_filled_frame)
 
     for (object_counter in all_objects) {
 
       # single out individual artefacts (starting with the largest)
-
       current_object <- EBImage::rmObjects(
         bin_image_labeled_filled_frame,
         all_objects[all_objects != object_counter]
       )
-      # EBImage::display(current_object,
-      #                  method="raster",
-      #                  interpolate=F)
 
       # inverts the image (black artefact on white background)
       current_object_inverted <- 1 - current_object
@@ -101,7 +67,11 @@ separate_single_artefacts <- function(pathname_input, pathname_output) {
       # save it as .jpg with a pseudo-number (does not represent the number which the artefact might have on the page)
       EBImage::writeImage(
         current_object_inverted,
-        paste0(pathname_output, "/", strsplit(files_to_use_names[current_masked_file], split = "[.]")[[1]][1], "_pseudo_no_", object_counter, ".jpg")
+        file.path(pathname_output,
+                  strsplit(files_to_use_names[current_masked_file], split = "[.]")[[1]][1],
+                  "_pseudo_no_",
+                  object_counter,
+                  ".jpg")
       )
     }
 
