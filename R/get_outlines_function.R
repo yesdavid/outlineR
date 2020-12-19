551c9d99a37b12853de276c14794daa960e66fe6
yesdavid <30009559+yesdavid@users.noreply.github.com>
2020-12-19 09:17:19 +0100

    option to rescale outlines according to rescale factor

--- R/get_outlines_function.R
+++ R/get_outlines_function.R
@@ -1,6 +1,6 @@
 #' get_outlines
 #'
-#' Uses Momocs::import_jpg1() to import outline coordinates from the single
+#' Uses Momocs::import_jpg() to import outline coordinates from the single
 #' .jpg files which are either already avaiable, or were created using the
 #' separate_single_artefacts_function function. This function preserves the
 #' filenames.
@@ -9,11 +9,16 @@
 #' from which the outlines should be derived (as for example created in the
 #' separate_single_artefacts_function function).
 #'
+#' @param tps_file_rescale (Default = NULL) A dataframe containing at least a column _IMAGE_ and a column _SCALE_.
+#' _IMAGE_ contains the names of the images, as they are written in *pathname_input*.
+#' _SCALE_ contains the scaling factor from pixel to a metric measurement.
+#' Such a file can be created in i.e. tpsDIG2 (Rohlf 2017).
+#'
 #' @return Returns a Momocs Out file containing the outlines' coordinates
 #' with their associated IDs, derived from the file name.
 #'
 #' @export
-get_outlines <- function(pathname_output) {
+get_outlines <- function(pathname_output, tps_file_rescale = NULL) {
   artefact_names <- list.files(pathname_output,
     pattern = ".jpg",
     full.names = F
@@ -27,11 +32,13 @@ get_outlines <- function(pathname_output) {
     outline_coordinates <- Momocs::import_jpg(file.path(pathname_output, artefact_names[input_counter]))
     out_file_single_outline <- Momocs::Out(outline_coordinates)
 
-    # Rescale coordinates from pixels to real length units
-    # current_tps_dataset <- subset(tps_file_regex, IMAGE == artefact_names[input_counter])
-    ## rescale using rescale factor
-    # out_file_single_outline <- Momocs::rescale(out_file_single_outline,
-    #                                            scaling_factor = current_tps_dataset$SCALE)
+    if (!is.null(tps_file_rescale)) {
+      # Rescale coordinates from pixels to real length units
+      current_tps_dataset <- subset(tps_file_rescale, IMAGE == strsplit(artefact_names[input_counter], split = "_pseudo")[[1]][1])
+      # rescale using rescale factor
+      out_file_single_outline <- Momocs::rescale(out_file_single_outline,
+                                                 scaling_factor = current_tps_dataset$SCALE)
+    }
 
     names(out_file_single_outline) <- strsplit(artefact_names[input_counter],
                                                split = "[.]")[[1]][1]
