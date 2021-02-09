7f12b7fd8eeafbe129e1db71d1cbf9f9707992d7
yesdavid <30009559+yesdavid@users.noreply.github.com>
2021-02-09 20:22:11 +0100

    description .tps file

---
+++ R/tps_file.R
@@ -0,0 +1,11 @@
+#' .tps file
+#'
+#' The file contains "LM", "IMAGE", "ID", and "SCALE" information (no more no less) for the *Morar_Quartz_Industry_Wellcome_Collection* image and was created using tpsUtil and tpsDig2 ([Rohlf 2017](http://www.sbmorphometrics.org/)).
+#'
+#' @format A file in .tps format containing "LM", "IMAGE", "ID", and "SCALE" information (no more no less) created using tpsUtil and tpsDig2 ([Rohlf 2017](http://www.sbmorphometrics.org/)).
+#'
+#'
+#'
+#'
+#'
+"tps_file.tps"
